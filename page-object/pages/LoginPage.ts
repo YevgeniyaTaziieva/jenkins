3e558993f9b41cc37c69fdfee161a600affa3aa3
studigrad <studigradilya@gmail.com>
2024-02-19 20:00:22 +0200

    add login tests

--- page-object/pages/LoginPage.ts
+++ page-object/pages/LoginPage.ts
@@ -1,19 +1,31 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../BasePage";
+import dotenv from "dotenv";
+dotenv.config();
 
 export class LoginPage extends BasePage {
   readonly emailField: Locator;
   readonly passField: Locator;
   readonly submitBtn: Locator;
+  readonly signInBtn: Locator;
+  readonly signInByEmailBtn: Locator;
+
   constructor(page: Page) {
     super(page);
-    this.emailField = page.locator("//input[@type='email']");
-    this.passField = page.locator("//input[@type='password']");
+    this.signInBtn = page.locator("(//button[@class='sc-iEYVpv dLIycx'])[1]");
+    this.signInByEmailBtn = page.locator(
+      "(//button[@class='sc-iEYVpv dWZiyj'])[2]"
+    );
+    this.emailField = page.locator("//input[@name='email']");
+    this.passField = page.locator("//input[@name='password']");
     this.submitBtn = page.locator("//button[@type='submit']");
   }
   async login() {
-    await this.emailField.fill("1337ilysha@gmail.com");
-    await this.emailField.fill("Test12345");
+    await this.page.goto(process.env.STAGE_URL as string);
+    await this.signInBtn.click();
+    await this.signInByEmailBtn.click();
+    await this.emailField.fill(process.env.EMAIL as string);
+    await this.passField.fill(process.env.PASSWORD as string);
     await this.submitBtn.click();
   }
 }
