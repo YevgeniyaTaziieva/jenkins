9ea1ddbf7a7d89abe57de137df849292e5af7a75
alexredko0004 <alexredko0004@gmail.com>
2024-02-20 22:10:47 +0200

    added goHome method to Navigation

--- page-object/components/Navigation.ts
+++ page-object/components/Navigation.ts
@@ -1,7 +1,8 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page, expect} from "@playwright/test";
 import { BasePage } from "../BasePage";
 
 export class Navigation {
+  readonly page: Page
   readonly createBtn: Locator;
   readonly homeIcon: Locator;
   readonly submitBtn: Locator;
@@ -9,12 +10,14 @@ export class Navigation {
   readonly signInByEmailBtn: Locator;
 
   constructor(page: Page) {
+    this.page = page
     this.createBtn = page.getByRole('button', { name: 'Create' });
     this.homeIcon = page.getByRole('link', { name: 'Kaggle' });
     
   }
   async goHome() {
     await this.homeIcon.click()
-    await this.createBtn.click()
+    expect(this.page.waitForURL('https://www.kaggle.com/'))
+    await expect(this.page.locator('//h1')).toContainText('Welcome')
   }
 }
