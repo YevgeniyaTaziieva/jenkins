ebf69bb41c583a08a7f859b2bed18fef9cb9fe68
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2024-02-21 18:15:33 +0200

    Datasets Page structure

--- page-object/BasePage.ts
+++ page-object/BasePage.ts
@@ -2,9 +2,15 @@ import { Locator, Page } from "@playwright/test";
 
 export class BasePage {
   readonly page: Page;
+  expectedUrlMainPage: string;
 
   constructor(page: Page) {
     this.page = page
+
+    this.expectedUrlMainPage = "https://www.kaggle.com";
+  }
+   async openMainPage(){
+    await this.page.goto(this.expectedUrlMainPage);
   }
 
 }
